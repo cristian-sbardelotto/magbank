67b8f1e95e0bff4d318cb0ab4acd495bce25b7b9
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-18 08:55:22 -0300

    feat(reactjs): added the first components and organized the files

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+const Hero = () => (
+  <Jumbotron>
+    <p>Pague suas contas pelo nosso APP</p>
+    <Button variant="outline-light">Abra sua conta</Button>
+  </Jumbotron>
+);
+
+export default Hero;
