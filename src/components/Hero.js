74ed680e3d567941626654f491ca5bba104e1beb
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-20 09:59:57 -0300

    feat(hero): added columns and row to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Button, Image } from 'react-bootstrap';
+import { Row, Col, Button, Image } from 'react-bootstrap';
 import logo from '../assets/logo__magic-pay.png'
 
 const Hero = () => (
-  <div className="jumbotron text-center">
-    <div className="container-fluid bg-light text-dark p-5">
-      <Image src={logo} />
-      <p>Pague suas contas pelo nosso APP</p>
-      <Button variant="outline-light">Abra sua conta</Button>
+  <div className="jumbotron text-center text-light">
+      <Row className="my-lg-5">
+        <Col lg className="text-lg-right my-lg-5">
+          <Image src={logo} />
+        </Col>
+        <Col lg className="text-lg-left my-lg-5">
+          <p>Pague suas contas pelo nosso APP</p>
+          <Button variant="outline-light">Abra sua conta</Button>
+        </Col>
+      </Row>
     </div>
-  </div>
 );
 
 export default Hero;
