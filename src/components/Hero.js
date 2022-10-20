d54e767f0e5475ccb6ce197322e94ec040aa4bb6
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-20 20:51:41 -0300

    feat(hero): finished hero style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Row, Col, Button, Image } from 'react-bootstrap';
-import logo from '../assets/logo__magic-pay.png'
+import logo from '../assets/logo__magic-pay--mobile.png';
+import logoDesktop from '../assets/logo__magic-pay.png';
+import './Hero.scss';
 
 const Hero = () => (
-  <div className="jumbotron text-center text-light">
+  <div className="jumbotron text-center text-light hero">
       <Row className="my-lg-5">
         <Col lg className="text-lg-right my-lg-5">
-          <Image src={logo} />
+          <Image className="d-lg-none text-center" src={logo} />
+          <Image className="d-none d-lg-inline-block" src={logoDesktop} />
         </Col>
-        <Col lg className="text-lg-left my-lg-5">
-          <p>Pague suas contas pelo nosso APP</p>
+        <Col lg className="text-lg-left my-lg-5 btn-mod">
+          <p className="p">Pague suas contas pelo nosso APP</p>
           <Button variant="outline-light">Abra sua conta</Button>
         </Col>
       </Row>
