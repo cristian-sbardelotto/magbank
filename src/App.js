ad3afa11e7f1046cb026e3474171e99841ea6458
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-04 07:59:47 -0300

    feat(dashboard): added the dashboard buttons to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AccountModal from './components/AccountModal';
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faCircle } from '@fortawesome/free-solid-svg-icons';
 import './Dashboard.scss';
 
 const Dashboard = () => (
-  <Container>
+  <Container className='dashboard py-5'>
     <Row>
-      <Col xs={12} lg={3}>
-        <Row>
-          <Col xs={4}>
-            <FontAwesomeIcon icon={faUser} size='4x' />
+      <Col xs={12} lg={4}>
+        <Row className='align-items-center mb-5'>
+          <Col xs={3}>
+            <span className='dashboard__user-avatar'>
+              <FontAwesomeIcon icon={faCircle} size='5x' color='#f8f9fa' />
+              <FontAwesomeIcon className='dashboard__user-icon' icon={faUser} size='3x' color='#7c7d7d' />
+            </span>
           </Col>
-          <Col xs={8}>
-            <h2>Cristian</h2>
+          <Col xs={9}>
+            <h4>Cristian Sbardelotto</h4>
             <p className='text-muted'>ag: 1234 c/: 4321-5</p>
           </Col>
         </Row>
+        <div className='d-grid gap-2'>
+          <Button className='dashboard__button dashboard__button--active text-start' variant='link' size='lg'>Minha conta</Button>
+          <Button className='dashboard__button text-start' variant='link' size='lg'>Pagamentos</Button>
+          <Button className='dashboard__button text-start' variant='link' size='lg'>Extrato</Button>
+        </div>
       </Col>
 
       <Col xs={12} lg={3}></Col>
-      <Col xs={12} lg={6}></Col>
+
+      <Col xs={12} lg={5}></Col>
     </Row>
   </Container>
 );
