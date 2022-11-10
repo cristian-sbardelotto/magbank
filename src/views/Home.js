973a92f339b064a3641068b40188fa4a2ecd673b
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-10 07:16:41 -0300

    fix(import): fixed the import errors in home and faq

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
 import Hero from '../components/Hero';
 import CreditCard from '../components/CreditCard';
 import CardList from '../components/CardList';
